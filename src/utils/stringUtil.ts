dea383dc332fc5022587deca0aecc8bc8e44e5ba
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-09-30 15:59:26 -0500

    feat: Add string utils

---
+++ src/utils/stringUtil.ts
@@ -0,0 +1,11 @@
+export const getRandomString = (length: number): string => {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+  let result = '';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
