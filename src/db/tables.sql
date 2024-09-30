2395f2d8f57431383586039af548117de50bf1b9
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-09-30 08:50:27 -0500

    feat: Add game room and lobby tables

--- src/db/tables.sql
+++ src/db/tables.sql
@@ -19,3 +19,26 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     REFERENCES users(id)
     ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS game_rooms (
+  room_id VARCHAR(255) PRIMARY KEY,
+  created_by VARCHAR(255),
+  current_problem_id INT,
+  current_round_start_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS game_room_users (
+  room_id VARCHAR(255) NOT NULL,
+  username VARCHAR(255),
+  profile_pic TEXT,
+  socket_id VARCHAR(255) DEFAULT 'undefined',
+  PRIMARY KEY (room_id, username, socket_id)
+);
+
+CREATE TABLE IF NOT EXISTS lobby_users (
+  username VARCHAR(255) NOT NULL,
+  profile_pic TEXT,
+  socket_id VARCHAR(255) DEFAULT 'undefined',
+  PRIMARY KEY (username, socket_id)
+);
