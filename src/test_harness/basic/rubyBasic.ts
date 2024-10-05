e23fad2ee5eaf96416562fc5575c39cf4288c67b
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-10-05 18:03:52 -0500

    feat: Add deep sort to Ruby basic harness

--- src/test_harness/basic/rubyBasic.ts
+++ src/test_harness/basic/rubyBasic.ts
@@ -3,14 +3,21 @@ import { jsonToRuby } from "../converter/converter";
 export const rubyBasicHarness = (code: string, args: any, testCases: any[], answerAnyOrder: boolean = false) => `
 ${code}
 
+def deep_sort(obj)
+  # Recursive function to sort arrays deeply
+  if obj.is_a?(Array)
+    obj.map { |el| deep_sort(el) }.sort
+  else
+    obj
+  end
+end
+
 def run_test_cases
-    # solution = Solution.new
     test_cases = ${jsonToRuby(testCases)}
     rbArgs = ${jsonToRuby(args)}
 
     test_cases.each_with_index do |test_case, i|
         args = rbArgs.keys.map { |arg| test_case[arg] }
-        # result = solution.solve(*args)
         result = solve(*args)
 
         # Submission
@@ -19,8 +26,8 @@ def run_test_cases
             answer_any_order = ${jsonToRuby(answerAnyOrder)}
 
             if answer_any_order && result.is_a?(Array) && expected_output.is_a?(Array)
-                result = result.sort
-                expected_output = expected_output.sort
+                result = deep_sort(result)
+                expected_output = deep_sort(expected_output)
             end
 
             if result != expected_output
