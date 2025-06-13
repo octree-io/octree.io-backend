e2036afeb7aa706a31d37ac907ac544026dd9a50
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2025-06-13 08:39:23 -0500

    chore: Remove auth from Practice Room endpoints

--- src/routes/practiceroom.routes.ts
+++ src/routes/practiceroom.routes.ts
@@ -1,11 +1,10 @@
 import express from "express";
-import { verifyToken } from "../middleware/jwtMiddleware";
 import { getHintForLeetcodeProblem, getLeetcodeProblem, runCode } from "../controllers/practiceroom.controller";
 
 const router = express.Router();
 
-router.post("/leetcode", verifyToken, getLeetcodeProblem);
-router.post("/hint", verifyToken, getHintForLeetcodeProblem);
-router.post("/run", verifyToken, runCode);
+router.post("/leetcode", getLeetcodeProblem);
+router.post("/hint", getHintForLeetcodeProblem);
+router.post("/run", runCode);
 
 export default router;
